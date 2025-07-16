6b56e7b720f241d541295ac31ace461e6aa40209
muhtegaralfikri <105841110722@student.unismuh.ac.id>
2025-07-16 21:33:02 +0800

    tugas_4

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,18 +8,16 @@ import { Text } from "react-native";
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  // Memuat semua font yang dibutuhkan untuk tugas
+
   const [loaded, error] = useFonts({
-    // --- 5 FONT STATIS ---
-    // (1 file = 1 gaya. Kita pakai yang Regular)
+    //FONT STATIS
     'PressStart': require("../assets/fonts/PressStart2P-Regular.ttf"),
     'PTSerif-Regular': require("../assets/fonts/PTSerif-Regular.ttf"),
     'Ubuntu-Regular': require("../assets/fonts/Ubuntu-Regular.ttf"),
     'Lato-Regular': require("../assets/fonts/Lato-Regular.ttf"),
     'Poppins-Regular': require("../assets/fonts/Poppins-Regular.ttf"),
 
-    // --- 5 FONT VARIABEL ---
-    // (1 file = semua gaya. Diatur via fontWeight)
+    //FONT VARIABEL
     'Inter-Variable': require("../assets/fonts/Inter-VariableFont_opsz,wght.ttf"),
     'Montserrat-Variable': require("../assets/fonts/Montserrat-VariableFont_wght.ttf"),
     'RobotoFlex-Variable': require("../assets/fonts/RobotoFlex-VariableFont_GRAD,XOPQ,XTRA,YOPQ,YTAS,YTDE,YTFI,YTLC,YTUC,opsz,slnt,wdth,wght.ttf"),
@@ -27,25 +25,24 @@ export default function RootLayout() {
     'Raleway-Variable': require("../assets/fonts/Raleway-VariableFont_wght.ttf"),
   });
 
-  // useEffect untuk mengelola kapan splash screen harus disembunyikan
+  
   useEffect(() => {
     if (loaded || error) {
       SplashScreen.hideAsync();
     }
   }, [loaded, error]);
 
-  // Jika font belum selesai dimuat dan tidak ada error, jangan tampilkan apa-apa
-  // Splash screen akan tetap terlihat
+  
   if (!loaded && !error) {
     return null;
   }
 
-  // Menangani jika ada error saat memuat font
+  
   if (error) {
     console.error("Gagal memuat font:", error);
     return <Text>Terjadi kesalahan saat memuat font.</Text>;
   }
 
-  // Jika font berhasil dimuat, tampilkan struktur navigasi aplikasi
+
   return <Stack />;
 }
